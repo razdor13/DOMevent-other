a04a6a1e3717853d735c9f6cf5f595b1abcbe355
Razdor13 <oneamaday@gmail.com>
2023-11-08 22:36:09 +0200

    mod js code big update v0.1

--- js/script.js
+++ js/script.js
@@ -1,20 +1,7 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
+    //wait for load content ,and then runs the script
     const movieDB = {
         movies: [
             "Логан",
@@ -37,12 +24,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     addForm.addEventListener("submit", (event) => {
         event.preventDefault();
-        const newFilm = addImput.value;
+        let newFilm = addImput.value;
         const favorite = checkBox.checked;
-        movieDB.movies.push(newFilm); //add film in arrey
-        sortArr(movieDB.movies);
 
-        createMovieList(movieDB.movies, movieList);
+        if (newFilm) {
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            if (favorite) {
+                console.log("Улюблений фільм");
+            }
+            movieDB.movies.push(newFilm); //add film in arrey
+            sortArr(movieDB.movies);
+            createMovieList(movieDB.movies, movieList);
+        }
+
         event.target.reset();
     });
 
@@ -62,6 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function createMovieList(films, parent) {
         parent.innerHTML = "";
+        sortArr(movieDB.movies);
         films.forEach((film, i) => {
             parent.innerHTML += `<li class="promo__interactive-item">${
                 i + 1
@@ -69,10 +66,17 @@ document.addEventListener("DOMContentLoaded", () => {
                                         <div class="delete"></div>
                                     </li>`;
         });
+
+        document.querySelectorAll(".delete").forEach((btn, i) => {
+            btn.addEventListener("click", () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                createMovieList(films, parent); // recurs
+            });
+        });
     }
 
     createMovieList(movieDB.movies, movieList);
     deleteAdv(adv);
     makeChenges();
-    sortArr(movieDB.movies);
 });
